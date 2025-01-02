2663a848ca463bda0a434a0c04fca46ebd94c65f
Kash Arcot <arcotk@google.com>
2025-01-02 16:12:41 -0600

    Gsdfg

--- execute_dvt/connections.sh
+++ execute_dvt/connections.sh
@@ -18,8 +18,13 @@ eval $bqcommand
 # tdcommand="data-validation connections add --connection-name td_conn Teradata --host $terahost --port $teraport --user-name $terauser --password $terapw"
 # eval $tdcommand
 
-echo "Testing FileSystem connection"
-fscommand="data-validation connections add --connection-name FILE_conn1 FileSystem --table-name file1_table --file-path gs://dvt_filesystem_conn_test/test_src.csv --file-type csv"
+echo "Testing FileSystem src connection"
+fscommand="data-validation connections add --connection-name FILE_conn1 FileSystem --table-name file1_table --file-path gs://dvt_filesystem_conn_test1/test_src.csv --file-type csv"
+echo $fscommand
+eval $fscommand
+
+echo "Testing FileSystem tgt connection"
+fscommand="data-validation connections add --connection-name FILE_conn2 FileSystem --table-name file2_table --file-path gs://dvt_filesystem_conn_test1/test_tgt.csv --file-type csv"
 echo $fscommand
 eval $fscommand
 
